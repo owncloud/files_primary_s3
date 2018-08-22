0eaffeefe28d61e3c1d2c417a375e7aa820a2b52
Thomas Müller <thomas.mueller@tmit.eu>
2018-08-22 16:03:09 +0200

    no path-style

--- tests/drone/ceph.config.php
+++ tests/drone/ceph.config.php
@@ -4,21 +4,20 @@ $CONFIG = [
 		'class' => 'OCA\Files_Primary_S3\S3Storage',
 		'arguments' => [
 			// replace with your bucket
-			'bucket' => 'OWNCLOUD',
-			'autocreate' => true,
+			'bucket' => 'owncloud',
 			// uncomment to enable server side encryption
 			//'serversideencryption' => 'AES256',
 			'options' => [
 				// version and region are required
 				'version' => '2006-03-01',
-				'region'  => 'us-central-1',
+				'region'  => 'us-east-1',
 				'credentials' => [
 					// replace key and secret with your credentials
 					'key' => 'owncloud123456',
 					'secret' => 'secret123456',
 				],
-				'use_path_style_endpoint' => true,
-				'endpoint' => 'http://ceph:80/',
+				'use_path_style_endpoint' => false,
+				'endpoint' => 'http://ceph:8080/',
 			],
 		],
 	],
