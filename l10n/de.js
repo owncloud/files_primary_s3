903204ba270c81a515df5f38474cb1dfae7ef4f9
Thomas Müller <thomas.mueller@tmit.eu>
2020-03-12 16:43:15 +0100

    German partial translation

---
+++ l10n/de.js
@@ -0,0 +1,8 @@
+OC.L10N.register(
+    "files_primary_s3",
+    {
+    "Connection options and bucket must be configured." : "Verbindungsoptionen und der Bucket müssen konfiguriert sein.",
+    "No S3 ObjectStore available" : "Kein S3 Objektspeicher verfügbar",
+    "Bucket <%s> does not exist." : "Bucket <%s> existiert nicht.."
+},
+"nplurals=2; plural=(n != 1);");
