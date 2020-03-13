06237229903db917cb607df82b17c3b48a6ea166
Thomas Müller <thomas.mueller@tmit.eu>
2020-03-13 16:38:37 +0000

    [tx] updated from transifex

---
+++ l10n/hr.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "files_primary_s3",
+    {
+    "Connection options and bucket must be configured." : "Opcije spajanja i kante moraju se konfigurirati.",
+    "No S3 ObjectStore available" : "Nema S3 ObjectStore",
+    "Bucket <%s> does not exist." : "Kanta <1%s> ne postoji.",
+    "Upload failed. Please ask you administrator to have a look at the log files for more details." : "Učitavanje neuspješno. Molimo Vas da se obratite Vašem administratoru da provjeri zapise logiranja za više detalja."
+},
+"nplurals=3; plural=n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2;");
---
+++ l10n/pt_BR.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "files_primary_s3",
+    {
+    "Connection options and bucket must be configured." : "As opções de conexão e a garantia devem ser configurados.",
+    "No S3 ObjectStore available" : "Nenhum S3 ObjectStore disponível",
+    "Bucket <%s> does not exist." : "A garantia <%s> não existe.",
+    "Upload failed. Please ask you administrator to have a look at the log files for more details." : "Falha no envio. Peça ao administrador que dê uma olhada nos arquivos de log para obter mais detalhes."
+},
+"nplurals=2; plural=(n > 1);");
