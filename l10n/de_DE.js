1ea73efc7536b4e509cc5a7c7d3c2c17033c088f
Thomas Müller <thomas.mueller@tmit.eu>
2020-03-14 03:36:51 +0000

    [tx] updated from transifex

--- l10n/de_DE.js
+++ l10n/de_DE.js
@@ -4,6 +4,6 @@ OC.L10N.register(
     "Connection options and bucket must be configured." : "Verbindungsoptionen und der Bucket müssen konfiguriert sein.",
     "No S3 ObjectStore available" : "Kein S3-Objektspeicher verfügbar",
     "Bucket <%s> does not exist." : "Bucket <%s> existiert nicht.",
-    "Upload failed. Please ask you administrator to have a look at the log files for more details." : "Hochladen fehlgeschlagen. Für mehr Details bitte den Administrator fragen, sich die Protokolldateien anzusehen."
+    "Upload failed. Please ask you administrator to have a look at the log files for more details." : "Das Hochladen ist fehlgeschlagen. Bitte kontaktieren Sie den Administrator und geben ihm den Hinweis die Protokolldateien zu kontrollieren."
 },
 "nplurals=2; plural=(n != 1);");
