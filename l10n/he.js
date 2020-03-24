d691c86e33d036d8a1f8331f066f9c204bb2cc37
Thomas Müller <thomas.mueller@tmit.eu>
2020-03-24 09:35:39 +0000

    [tx] updated from transifex

---
+++ l10n/he.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "files_primary_s3",
+    {
+    "Connection options and bucket must be configured." : "יש להגדיר אפשרויות חיבור ומאגר נתונים.",
+    "No S3 ObjectStore available" : "אין S3 ObjectStore זמין",
+    "Bucket <%s> does not exist." : "מאגר נתונים <%s> אינו קיים.",
+    "Upload failed. Please ask you administrator to have a look at the log files for more details." : "העלאה נכשלה. יש לבקש מהאדמיניסטרטור שלך לבדוק את קובצי הלוג לפרטים נוספים."
+},
+"nplurals=4; plural=(n == 1 && n % 1 == 0) ? 0 : (n == 2 && n % 1 == 0) ? 1: (n % 10 == 0 && n % 1 == 0 && n > 10) ? 2 : 3;");
