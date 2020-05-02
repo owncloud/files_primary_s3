afd5140cbfc98a843f3bd552725c295d032a7cb5
Thomas Müller <thomas.mueller@tmit.eu>
2020-05-02 03:10:16 +0000

    [tx] updated from transifex

---
+++ l10n/es.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "files_primary_s3",
+    {
+    "Connection options and bucket must be configured." : "Se deben configurar las opciones de conexión y de bucket.",
+    "No S3 ObjectStore available" : "No hay disponible ningún S3 ObjectStore",
+    "Bucket <%s> does not exist." : "El bucket <%s> no existe.",
+    "Upload failed. Please ask you administrator to have a look at the log files for more details." : "Falló la subida. Por favor pida a su administrador que verifique los archivos de registro para mas detalles."
+},
+"nplurals=2; plural=(n != 1);");
