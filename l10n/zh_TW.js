7838e077509244301f2fe2f3b921958d78555cf4
Thomas Müller <thomas.mueller@tmit.eu>
2020-10-02 00:05:53 +0000

    [tx] updated from transifex

---
+++ l10n/zh_TW.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "files_primary_s3",
+    {
+    "Connection options and bucket must be configured." : "必須配置連接選項和存儲桶。",
+    "No S3 ObjectStore available" : "沒有可用的S3 ObjectStore",
+    "Bucket <%s> does not exist." : "不<%s>存在。",
+    "Upload failed. Please ask you administrator to have a look at the log files for more details." : "上傳失敗。您要求管理員查看記錄文件以獲取更多詳細信息。"
+},
+"nplurals=1; plural=0;");
