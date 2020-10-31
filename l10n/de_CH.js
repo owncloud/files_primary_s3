41577101eb703e09488f86c32d62d183304d5bcd
Thomas Müller <thomas.mueller@tmit.eu>
2020-10-31 00:05:17 +0000

    [tx] updated from transifex

---
+++ l10n/de_CH.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "files_primary_s3",
+    {
+    "Connection options and bucket must be configured." : "Verbindungsoptionen und der Bucket müssen konfiguriert sein.",
+    "No S3 ObjectStore available" : "Kein S3-Objektspeicher verfügbar",
+    "Bucket <%s> does not exist." : "Bucket <%s> existiert nicht.",
+    "Upload failed. Please ask you administrator to have a look at the log files for more details." : "Das Hochladen ist fehlgeschlagen. Bitte kontaktieren deinen Administrator und gib ihm den Hinweis die Protokolldateien zu kontrollieren."
+},
+"nplurals=2; plural=(n != 1);");
