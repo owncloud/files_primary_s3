93d4e0bc0e4f7994c1693d07b6bf98faa4d76e31
Lukas Hirt <lhirt@owncloud.com>
2021-10-13 00:05:23 +0000

    [tx] updated from transifex

--- l10n/sq.js
+++ l10n/sq.js
@@ -4,6 +4,7 @@ OC.L10N.register(
     "Connection options and bucket must be configured." : "Mundësitë e lidhjes dhe bucket-i duhen formësuar.",
     "No S3 ObjectStore available" : "S’ka S3 ObjectStore",
     "Bucket <%s> does not exist." : "Bucket-i <%s> nuk ekziston.",
-    "Upload failed. Please ask you administrator to have a look at the log files for more details." : "Ngarkimi dështoi. Ju lutemi, kërkojini përgjegjësit tuaj të shohë për më tepër hollësi te kartelat e regjistrit."
+    "Upload failed. Please ask you administrator to have a look at the log files for more details." : "Ngarkimi dështoi. Ju lutemi, kërkojini përgjegjësit tuaj të shohë për më tepër hollësi te kartelat e regjistrit.",
+    "Storage encryption is not compatible with S3 Object Storage." : "Fshehtëzimi i depozitës s’është i përputhshëm me S3 Object Storage."
 },
 "nplurals=2; plural=(n != 1);");
