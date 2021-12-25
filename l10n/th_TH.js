890e9560cf25d04dc5f813fa31e6fb0b5d19c02f
ownClouders <devops@owncloud.com>
2021-12-25 00:07:34 +0000

    [tx] updated from transifex

--- l10n/th_TH.js
+++ l10n/th_TH.js
@@ -4,6 +4,7 @@ OC.L10N.register(
     "Connection options and bucket must be configured." : "ต้องกำหนดค่าตัวเลือกการเชื่อมต่อและที่เก็บข้อมูล",
     "No S3 ObjectStore available" : "ไม่พบ S3 ObjectStore",
     "Bucket <%s> does not exist." : "ไม่พบ Bucket <%s> ",
-    "Upload failed. Please ask you administrator to have a look at the log files for more details." : "การอัปโหลดล้มเหลว โปรดขอให้ผู้ดูแลระบบของคุณ ดูรายละเอียดเพิ่มเติมได้จากไฟล์ log"
+    "Upload failed. Please ask you administrator to have a look at the log files for more details." : "การอัปโหลดล้มเหลว โปรดขอให้ผู้ดูแลระบบของคุณ ดูรายละเอียดเพิ่มเติมได้จากไฟล์ log",
+    "Storage encryption is not compatible with S3 Object Storage." : "การเข้ารหัสที่เก็บข้อมูลไม่เข้ากันกับ S3 Object Storage"
 },
 "nplurals=1; plural=0;");
