69c25d1f6fa652457961109c15767c652d3f9082
ownClouders <devops@owncloud.com>
2023-03-13 00:42:53 +0000

    [tx] updated from transifex

---
+++ l10n/en_GB.js
@@ -0,0 +1,10 @@
+OC.L10N.register(
+    "files_primary_s3",
+    {
+    "Connection options and bucket must be configured." : "Connection options and bucket must be configured.",
+    "No S3 ObjectStore available" : "No S3 ObjectStore available",
+    "Bucket <%s> does not exist." : "Bucket <%s> does not exist.",
+    "Upload failed. Please ask you administrator to have a look at the log files for more details." : "Upload failed. Please ask you administrator to have a look at the log files for more details.",
+    "Storage encryption is not compatible with S3 Object Storage." : "Storage encryption is not compatible with S3 Object Storage."
+},
+"nplurals=2; plural=(n != 1);");
