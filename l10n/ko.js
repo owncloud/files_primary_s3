63c844c1e906499742cf0595ebfda948f46c029e
ownClouders <devops@owncloud.com>
2023-03-24 00:59:10 +0000

    [tx] updated from transifex

---
+++ l10n/ko.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "files_primary_s3",
+    {
+    "Connection options and bucket must be configured." : "연결 옵션과 버킷은 반드시 구성되어야 합니다.",
+    "No S3 ObjectStore available" : "사용 가능한 S3 ObjectStore 없음",
+    "Bucket <%s> does not exist." : "<%s> 버킷이 존재하지 않습니다.",
+    "Upload failed. Please ask you administrator to have a look at the log files for more details." : "업로드에 실패했습니다. 자세한 내용은 관리자에게 로그 파일을 살펴보도록 요청하세요."
+},
+"nplurals=1; plural=0;");
