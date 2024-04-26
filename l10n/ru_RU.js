554f3616e1c8d717ab92ccf5d00bff0c6d23d25a
ownClouders <devops@owncloud.com>
2024-04-26 00:42:17 +0000

    [tx] updated from transifex

---
+++ l10n/ru_RU.js
@@ -0,0 +1,10 @@
+OC.L10N.register(
+    "files_primary_s3",
+    {
+    "Connection options and bucket must be configured." : "Должны быть заданы настройки соединения и контейнер.",
+    "No S3 ObjectStore available" : "Недоступно хранилище S3 ObjectStore",
+    "Bucket <%s> does not exist." : "Контейнер <%s> не существует.",
+    "Upload failed. Please ask you administrator to have a look at the log files for more details." : "Закачка не удалась. Для подробностей, пожалуйста, попросите вашего администратора заглянуть в файлы журнала.",
+    "Storage encryption is not compatible with S3 Object Storage." : "Зашифрованное хранение не совместимо с объектным хранилищем S3."
+},
+"nplurals=4; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<12 || n%100>14) ? 1 : n%10==0 || (n%10>=5 && n%10<=9) || (n%100>=11 && n%100<=14)? 2 : 3);");
