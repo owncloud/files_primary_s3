86fbf880fb8edc764f4c748c4bfb3c6e94209c79
ownClouders <devops@owncloud.com>
2024-10-14 01:39:01 +0000

    [tx] updated from transifex

---
+++ l10n/ug.js
@@ -0,0 +1,10 @@
+OC.L10N.register(
+    "files_primary_s3",
+    {
+    "Connection options and bucket must be configured." : "ئۇلىنىش تاللانمىلىرى ۋە چېلەكلەرنى چوقۇم تەڭشەش كېرەك.",
+    "No S3 ObjectStore available" : "S3 ObjectStore يوق",
+    "Bucket <%s> does not exist." : "چېلەك <% s> مەۋجۇت ئەمەس.",
+    "Upload failed. Please ask you administrator to have a look at the log files for more details." : "يۈكلەش مەغلۇپ بولدى. تېخىمۇ كۆپ تەپسىلاتلار ئۈچۈن باشقۇرغۇچىدىن خاتىرە ھۆججەتلىرىنى كۆرۈپ بېقىشىڭىزنى سورايمىز.",
+    "Storage encryption is not compatible with S3 Object Storage." : "ساقلاش مەخپىيلەشتۈرۈش S3 ئوبيېكت ساقلاشقا ماس كەلمەيدۇ."
+},
+"nplurals=2; plural=(n != 1);");
