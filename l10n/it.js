4bf8e9152c14a6ce928e61460c3bdbfe82f4fdc3
ownClouders <devops@owncloud.com>
2024-12-04 00:41:15 +0000

    [tx] updated from transifex

---
+++ l10n/it.js
@@ -0,0 +1,10 @@
+OC.L10N.register(
+    "files_primary_s3",
+    {
+    "Connection options and bucket must be configured." : "Le opzioni di connessione e il bucket devono essere configurati.",
+    "No S3 ObjectStore available" : "Nessun ObjectStore S3 disponibile",
+    "Bucket <%s> does not exist." : "Il Bucket <%s> non esiste.",
+    "Upload failed. Please ask you administrator to have a look at the log files for more details." : "Caricamento non riuscito. Chiedi all'amministratore di dare un'occhiata ai file di registro per maggiori dettagli.",
+    "Storage encryption is not compatible with S3 Object Storage." : "La crittografia dello storage non è compatibile con S3 Object Storage."
+},
+"nplurals=3; plural=n == 1 ? 0 : n != 0 && n % 1000000 == 0 ? 1 : 2;");
