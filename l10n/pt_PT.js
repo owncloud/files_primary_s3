13eb49b097c5f264865d9003399a898e0509c3b0
ownClouders <devops@owncloud.com>
2025-12-02 00:07:01 +0000

    [tx] updated from transifex

---
+++ l10n/pt_PT.js
@@ -0,0 +1,10 @@
+OC.L10N.register(
+    "files_primary_s3",
+    {
+    "Connection options and bucket must be configured." : "As opções de conexão e a garantia devem ser configurados.",
+    "No S3 ObjectStore available" : "Nenhum S3 ObjectStore disponível",
+    "Bucket <%s> does not exist." : "A garantia <%s> não existe.",
+    "Upload failed. Please ask you administrator to have a look at the log files for more details." : "Falha no envio. Peça ao administrador que dê uma olhada nos arquivos de log para obter mais detalhes.",
+    "Storage encryption is not compatible with S3 Object Storage." : "A criptografia de armazenamento não é compatível com o Armazenamento de Objetos S3."
+},
+"nplurals=3; plural=(n == 0 || n == 1) ? 0 : n != 0 && n % 1000000 == 0 ? 1 : 2;");
--- l10n/zh_TW.js
+++ l10n/zh_TW.js
@@ -4,6 +4,7 @@ OC.L10N.register(
     "Connection options and bucket must be configured." : "必須配置連接選項和存儲桶。",
     "No S3 ObjectStore available" : "沒有可用的S3 ObjectStore",
     "Bucket <%s> does not exist." : "不<%s>存在。",
-    "Upload failed. Please ask you administrator to have a look at the log files for more details." : "上傳失敗。您要求管理員查看記錄文件以獲取更多詳細信息。"
+    "Upload failed. Please ask you administrator to have a look at the log files for more details." : "上傳失敗。您要求管理員查看記錄文件以獲取更多詳細信息。",
+    "Storage encryption is not compatible with S3 Object Storage." : "存储加密与 S3 对象存储不兼容。"
 },
 "nplurals=1; plural=0;");
